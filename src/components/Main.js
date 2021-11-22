66e3baff50169ce03721be592e1cdfb36e930064
emzakh <massimino.gutierrezmantione@gmail.com>
2021-11-22 21:09:17 +0100

    correction transition darkDiv, suppression height:100%

--- src/components/Main.js
+++ src/components/Main.js
@@ -123,12 +123,13 @@ const DarkDiv = styled.div`
   background-color: #000;
   bottom: 0;
   right: 50%;
-  width: ${(props) => (props.click ? "50%" : "0%")};
-  height: ${(props) => (props.click ? "100%" : "0%")};
-  height: 100%;
+  width: ${props => props.click ? "50%" : "0%"};
+  height: ${props => props.click ? "100%" : "0%"};  
   z-index: 1;
   transition: height 0.5s ease, width 1s ease 0.5s;
-`;
+`
+
+
 
 const Main = () => {
   const [click, setClick] = useState(false);
