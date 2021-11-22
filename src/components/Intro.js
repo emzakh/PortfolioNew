6098bb3298dc7b1c7cac137c101ada8ea792e872
emzakh <massimino.gutierrezmantione@gmail.com>
2021-11-22 20:51:35 +0100

    ajout framer-motion sur intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import {motion} from 'framer-motion'
 import Me from '../assets/Images/profile-img.png'
 
-const Box = styled.div`
+const Box = styled(motion.div)`
 position: absolute;
 left: 50%;
 top: 50%;
@@ -11,7 +11,6 @@ transform: translate(-50%, -50%);
 
 
 width: 65vw;
-height:45vh;
 display: flex;
 
 background: linear-gradient(
@@ -61,19 +60,27 @@ justify-content: space-evenly;
 
 const Intro = () => {
     return (
-        <Box>
+        <Box
+        initial={{height:0}}
+        animate={{height:'40vh'}}
+        transition={{ type: 'spring', duration:2, delay:1 }}
+        >
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm Maximino</h3>
-                    <h6>J'aime jouer aux jeux-vidéos jusqu'à 6h du matin me lever à 14h</h6>
+                    <h3>I'm Maximino.</h3>
+                    <h6>J'aime jouer aux jeux-vidéos jusqu'à 6h du matin me lever à 14h.</h6>
                 </Text>
             </SubBox>
             
             <SubBox>
-                <div>
+                <motion.div
+                  initial={{opacity:0}}
+                  animate={{opacity:1}}
+                  transition={{ duration:2, delay:2 }}
+                  >
                     <img className="pic" src={Me} alt="Profile Pic" />
-                </div>
+                </motion.div>
             </SubBox>
         </Box>
     )
