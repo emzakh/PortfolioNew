c13cddf14778c6b672e728a18a5e2cfa156c0d36
emzakh <massimino.gutierrezmantione@gmail.com>
2021-11-23 20:28:39 +0100

    blogpage skeleton

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
+import LogoComponent from '../subComponents/LogoComponent'
+import PowerButton from '../subComponents/PowerButton'
+import SocialIcons from '../subComponents/SocialIcons'
 
-function BlogPage() {
+const MainContainer = styled.div`
+background-image: url(${img}); 
+background-size:cover;
+background-repeat:no-repeat;
+background-attachment: fixed;
+background-position:center;
+width:100vw;
+`
+
+const Container = styled.div`
+background-color:${props=>`rgba(${props.theme.bodyRgba},0.8)`};
+width:100%;
+height:100vh;
+position:relative;
+padding-bottom:5rem;
+`
+
+const Center = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+padding-top:10rem;
+`
+
+const Grid = styled.div`
+display:grid;
+grid-template-columns:repeat(2, minmax(calc(10rem+15vw), 1fr));
+grid-gap: calc(1rem + 2vw);
+`
+
+
+const BlogPage = () => {
     return (
-        <div>
-            BlogPage Component/Page
-        </div>
+     <MainContainer>
+        <Container>
+            <LogoComponent/>
+            <PowerButton/>
+            <SocialIcons/>
+            <Center>
+                <Grid>
+                    <h1>Blog</h1>
+                    <h1>Blog</h1>
+                    <h1>Blog</h1>
+                    <h1>Blog</h1>
+                </Grid>
+            </Center>
+        </Container>
+
+     </MainContainer>
     )
 }
 
