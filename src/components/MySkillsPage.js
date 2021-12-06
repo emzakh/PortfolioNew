24e39ca6ae1589695e7e2f573eb475d045f22717
emzakh <massimino.gutierrezmantione@gmail.com>
2021-12-06 11:04:24 +0100

    responsive myskills & correction css

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./Themes";
+import { lightTheme, mediaQueries } from "./Themes";
 import styled from "styled-components";
 import { Design, Dev } from "./AllSvgs";
-import ParticleComponent from '../subComponents/ParticleComponent';
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-
+import ParticleComponent from "../subComponents/ParticleComponent";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
 
 const Box = styled.div`
   background-color: ${(props) => props.theme.body};
@@ -16,6 +15,24 @@ const Box = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+
+  ${mediaQueries(50)`
+  flex-direction:column;  
+  padding:8rem 0;
+  height:auto;
+  &>*:nth-child(4){
+    margin-bottom:5rem;
+  }
+ 
+`};
+  ${mediaQueries(30)`
+ 
+  &>*:nth-child(4){
+    margin-bottom:4rem;
+    
+  }
+ 
+`};
 `;
 const Main = styled.div`
   border: 2px solid ${(props) => props.theme.text};
@@ -26,17 +43,28 @@ const Main = styled.div`
   height: 60vh;
   z-index: 3;
   line-height: 1.5;
-  cursor:pointer;
+  cursor: pointer;
 
-  font-famuily: "Ubuntu", monospace;
+  font-family: "Ubuntu Mono", monospace;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-  &:hover{
-      color: ${(props) => props.theme.body};
-      background-color: ${(props) => props.theme.text};
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
   }
+
+  ${mediaQueries(60)`
+  height: 55vh;
+`};
+
+  ${mediaQueries(50)`
+    width: 50vw;
+    height: max-content;
+
+`};
 `;
 
 const Title = styled.h2`
@@ -45,68 +73,98 @@ const Title = styled.h2`
   align-items: center;
   font-size: calc(1em + 1vw);
 
+  ${mediaQueries(60)`
+  font-size:calc(0.8em + 1vw);
+`};
+
+${mediaQueries(50)`
+  font-size:calc(1em + 2vw);
+  margin-bottom:1rem;
+`};
+
+${mediaQueries(30)`
+              font-size:calc(1em + 1vw);
+`};
+${mediaQueries(25)`
+              font-size:calc(0.8em + 1vw);
+              svg{
+                width:30px;
+                height:30px;
+              }
+`};
+
   ${Main}:hover{
     &>*{
         fill: ${(props) => props.theme.body};
 
     }
 }
-&:*>:first-child{
+  & > *:first-child {
     margin-right: 1rem;
-}
+  }
+
+
 `;
 
 const Description = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
+  
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
 
-color: ${(props) => props.theme.text};
-font-size: calc(0.6em + 1vw);
-padding :0.5rem 0;
-${Main}:hover & { 
-  color: ${(props) => props.theme.body};
-}
+  ${mediaQueries(50)`
+        font-size: calc(0.8em + 1vw);
+  `};
 
+  ${mediaQueries(30)`
+        font-size:calc(0.7em + 1vw);  
+  `};
 
+  ${mediaQueries(25)`
+        font-size:calc(0.5em + 1vw);
+  `};
 
-strong{
-    margin-bottom:1rem;
+  strong {
+    margin-bottom: 1rem;
     text-transform: uppercase;
-}
-ul,p{
+  }
+  ul,
+  p {
     margin-left: 2rem;
-}
+  }
 `;
-
-
 
 const MySkillsPage = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <Box>
-
-        <LogoComponent theme='light' />
-        <SocialIcons theme='light'/> 
-        <ParticleComponent theme='light' />
-        
+        <LogoComponent theme="light" />
+        <SocialIcons theme="light" />
+        <ParticleComponent theme="light" />
 
         <Main>
           <Title>
             <Design width={40} height={40} /> Designer
           </Title>
           <Description>
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quasi consequatur voluptates? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            quasi consequatur voluptates?
           </Description>
           <Description>
-              <strong>I like to Design</strong>
-              <ul>
-                <li>Web Design</li>
-                <li>Mobile Apps</li>
-              </ul> 
+            <strong>I like to Design</strong>
+            <ul>
+              <li>Web Design</li>
+              <li>Mobile Apps</li>
+            </ul>
           </Description>
           <Description>
-              <strong>Tools</strong>
-              <ul>
-                <li>Figma</li>                
-              </ul> 
+            <strong>Tools</strong>
+            <ul>
+              <li>Figma</li>
+            </ul>
           </Description>
         </Main>
         <Main>
@@ -114,19 +172,21 @@ const MySkillsPage = () => {
             <Dev width={40} height={40} /> Frontend Developer
           </Title>
           <Description>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur praesentium mollitia a.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur praesentium mollitia a.
           </Description>
           <Description>
-          <strong>Skills</strong>
-          <p>
-              Html, CSS, Javascript, React, Symfony, mySQL, Api-Platform, Wordpress ...
-          </p>
-           </Description>
-           <Description>
-              <strong>Tools</strong>
-              <ul>
-                <li>VScode, PHPStorm, Github, WAMP, ...</li>                
-              </ul> 
+            <strong>Skills</strong>
+            <p>
+              Html, CSS, Javascript, React, Symfony, mySQL, Api-Platform,
+              Wordpress ...
+            </p>
+          </Description>
+          <Description>
+            <strong>Tools</strong>
+            <ul>
+              <li>VScode, PHPStorm, Github, WAMP, ...</li>
+            </ul>
           </Description>
         </Main>
       </Box>
